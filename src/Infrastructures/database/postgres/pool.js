4399bad808aaf79f82f5c017e54e2e1ae6054b50
lazuardi <bintanglazuardi70@gmail.com>
2024-11-07 15:45:48 +0700

    add ci cd and deployment file configuration

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -9,6 +9,18 @@ const testConfig = {
   database: process.env.PGDATABASE_TEST,
 };
 
-const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
+const config = {
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool(config);
 
 module.exports = pool;
