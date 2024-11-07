08bf6cc28da9541ffa22f9534d2d2dffcc5b0644
lazuardi <bintanglazuardi70@gmail.com>
2024-11-07 21:24:32 +0700

    update pool config

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -10,11 +10,6 @@ const testConfig = {
 };
 
 const config = {
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
